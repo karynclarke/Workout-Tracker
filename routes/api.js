f45169eadcea333650afe1e6d6b44399268afb57
karynclarke <karynclarke@comcast.net>
2020-09-04 20:03:59 -0700

    db connect

--- routes/api.js
+++ routes/api.js
@@ -6,12 +6,20 @@ router.post("/api/workouts", (req, res) => {
     Workout.create({}).then(dbWorkout => {
             res.json(dbWorkout)
         })
-        .catch(err)
-})
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
-
-
-
-
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 module.exports = router;
