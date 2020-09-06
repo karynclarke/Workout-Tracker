5306b48c700ad9cb7939511cd62ff05c5da28ead
karynclarke <karynclarke@comcast.net>
2020-09-05 20:05:10 -0700

    router

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 // add api routes
-router.post("/api/workouts", (req, res) => {
-    Workout.create({}).then(dbWorkout => {
+// gets all workouts
+router.get("/", (req, res) => {
+    Workout.find()
+        .then(dbWorkout => {
             res.json(dbWorkout)
         })
         .catch(err => {
@@ -11,6 +13,30 @@ router.post("/api/workouts", (req, res) => {
         });
 });
 
+router.put(":id/api/workouts", (req, res) => {
+    Workout.update({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// adds new workout
+router.post("/api/workouts", ({
+    body
+}, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// updates workouts
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
         .sort({ date: -1 })
@@ -22,4 +48,15 @@ router.get("/api/workouts", (req, res) => {
         });
 });
 
+// controls number of workouts
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+        .then(dbWorkouts => {
+            console.log(dbWorkouts)
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 module.exports = router;
